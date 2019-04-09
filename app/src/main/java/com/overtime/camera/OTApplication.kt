83357b343ccd66fa904545f13b9f8592e63eb291
OViscarra <34140144+OViscarra@users.noreply.github.com>
2019-04-09 11:52:24 -0400

    init project

---
+++ app/src/main/java/com/overtime/camera/OTApplication.kt
@@ -0,0 +1,20 @@
+package com.overtime.camera
+
+import android.app.Application
+import android.content.res.Configuration
+
+class OTApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+
+    }
+
+
+}
